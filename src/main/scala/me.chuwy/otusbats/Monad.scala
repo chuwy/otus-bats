1ccd68ad3cdaf5b6b085d1f2f2cc34c173180346
Oleksandr Korotkyi <oleksandr.korotkyi@oracle.com>
2022-01-18 14:17:57 +0200

    Methods/classes implemented

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -5,9 +5,53 @@ trait Monad[F[_]] extends Functor[F] { self =>
 
   def point[A](a: A): F[A]
 
-  def flatten[A](fa: F[F[A]]): F[A]
+  def flatten[A](fa: F[F[A]]): F[A] = flatMap(fa)(a => a)
+
 }
 
 object Monad {
+
+  // instances
+  implicit val optionMonad: Monad[Option] = new Monad[Option] {
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+
+    override def point[A](a: A): Option[A] = Some(a)
+
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+
+  }
+
+  implicit val listMonad: Monad[List] = new Monad[List] {
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+
+    override def point[A](a: A): List[A] = List(a)
+
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  implicit val functon0Monad: Monad[Function0] = new Monad[Function0] {
+    override def flatMap[A, B](fa: () => A)(f: A => () => B): () => B = f(fa())
+
+    override def point[A](a: A): () => A = () => a
+
+    override def map[A, B](fa: () => A)(f: A => B): () => B = () => f(fa())
+  }
+
+  // summoner
+  implicit def apply[F[_]](implicit ev: Monad[F]): Monad[F] = ev
+
+  // syntax
+  implicit class MonadOps[F[_], A](fa: F[A]) {
+    def map[B](f: A => B)(implicit ev: Monad[F]): F[B] = ev.map(fa)(f)
+    def flatMap[B](f: A => F[B])(implicit ev: Monad[F]): F[B] = ev.flatMap(fa)(f)
+  }
+
+  implicit class MonadOpsRaw[A](a: A){
+    def pure[F[_]](implicit ev: Monad[F]): F[A] = ev.point(a)
+  }
+
+  implicit class NestedMonadOps[F[_], A](fa: F[F[A]]) {
+    def flatten(implicit ev: Monad[F]): F[A] = ev.flatten(fa)
+  }
 
 }
--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -8,40 +8,44 @@ trait Show[A] {
 object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
-
+  implicit val intShow: Show[Int] = fromJvm
+  implicit val stringShow: Show[String] = s => s
+  implicit val booleanShow: Show[Boolean] = v => if (v) "true" else "false"
 
   // 1.2 Instances with conditional implicit
 
-  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] = (a: List[A]) => a.map(ev.show).mkString
+
+  implicit def setShow[A](implicit ev: Show[A]): Show[Set[A]] = (a: Set[A]) => a.map(ev.show).mkString
 
 
   // 2. Summoner (apply)
+  def apply[A](implicit ev: Show[A]) = ev
+
+
+  /** Transform list of `A` into `String` with custom separator, beginning and ending.
+   *  For example: "[a, b, c]" from `List("a", "b", "c")`
+   *
+   *  @param separator. ',' in above example
+   *  @param begin. '[' in above example
+   *  @param end. ']' in above example
+   */
+  def mkString_[A](list: List[A], separator: String, begin: String, end: String)(implicit ev: Show[A]): String =
+    list.map(ev.show).mkString(begin, separator, end)
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
     def show(implicit ev: Show[A]): String =
-      ???
-
-    /** Transform list of `A` into `String` with custom separator, beginning and ending.
-     *  For example: "[a, b, c]" from `List("a", "b", "c")`
-     *
-     *  @param separator. ',' in above example
-     *  @param begin. '[' in above example
-     *  @param end. ']' in above example
-     */
-    def mkString_(list: List[A], separator: String, begin: String, end: String)(implicit ev: Show[A]): String =
-      ???
-
+      ev.show(a)
   }
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
+  def fromJvm[A]: Show[A] = (a: A) => a.toString
   
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = (a: A) => f(a)
 
 }
