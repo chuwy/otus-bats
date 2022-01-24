9bb24e45b451d83d20baa5747398315c2d1cacb3
Valentin Kostiuk <valentin.kostiuk@gmail.com>
2022-01-24 23:52:53 +0200

    reverting functor file to original state

--- src/main/scala/me.chuwy/otusbats/Functor.scala
+++ src/main/scala/me.chuwy/otusbats/Functor.scala
@@ -6,4 +6,29 @@ trait Functor[F[_]] {
 
 object Functor {
 
+  // *
+  def a: Int = 3
+
+  // * -> *
+  def b(a: Int): Int = ???
+
+  // (* -> *) -> *
+  def c(f: Int => Int): Int = ???
+
+  // * -> * -> *
+  def d(a: Int, b: Int): Int = ???
+
+
+  val example: Option[Int] = Some(10)
+  example.map(_ + 10)
+
+  case class NeFunctor[A](a: A, counter: Int)
+
+  implicit val neFunctorFunctor: Functor[NeFunctor] = new Functor[NeFunctor] {
+    def map[A, B](fa: NeFunctor[A])(f: A => B): NeFunctor[B] =
+      NeFunctor(f(fa.a), fa.counter + 1)
+  }
+
+  // a.map(f).map(indentity) === a.map(f)
+
 }
