87aa262cb6a52ee40593c52696b976f4ee47b6d2
Sergey Gribkov <63419565+gribkov-s@users.noreply.github.com>
2022-08-03 02:41:59 +0300

    Update Show.scala

--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -8,21 +8,29 @@ trait Show[A] {
 object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
+  
+  def from[T](f: T => String): Show[T] = (a: T) => f(a)
 
+  implicit val intShow: Show[Int] = from[Int](_.toString)
+  implicit val stringShow: Show[String] = from[String](x => x)
+  implicit val booleanShow: Show[Boolean] = from[Boolean](_.toString)
 
   // 1.2 Instances with conditional implicit
 
   implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
-
+    (a: List[A]) => a.mkString(" ")
+  
+  implicit def setShow[A](implicit ev: Show[A]): Show[Set[A]] =
+    (a: Set[A]) => a.mkString(" ")
 
   // 2. Summoner (apply)
+  
+  def apply[A](implicit ev: Show[A]): Show[A] = ev
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
-    def show(implicit ev: Show[A]): String =
-      ???
+    def show(implicit ev: Show[A]): String = ev.show(a)
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -40,15 +48,15 @@ object Show {
    *  @param end. ']' in above example
    */
   def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    ???
+    list.mkString(begin, separator, end)
 
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
+  def fromJvm[A]: Show[A] = (a: A) => a.toString
   
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = from(f)
 
 }
