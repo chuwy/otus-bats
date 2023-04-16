781650c24a6a7c7953b1f71fcab9fb3d54472315
Юлия Хегай <daedne@Ulias-MacBook-Air.local>
2023-04-16 21:48:47 +0300

    дз по уроку 13

--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -9,20 +9,35 @@ object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
 
+  implicit val stringShow: Show[String] = Show.fromJvm[String]
+  implicit val stringShow2: Show[String] = Show.fromFunction[String](a => a.toString)
+
+  implicit val intShow: Show[Int] = Show.fromJvm[Int]
+  implicit val intShow2: Show[Int] = Show.fromFunction[Int](a => a.toString)
+
+  implicit val booleanShow: Show[Boolean] = Show.fromJvm[Boolean]
+  implicit val booleanShow2: Show[Boolean] = Show.fromFunction[Boolean](a => a.toString)
+
 
   // 1.2 Instances with conditional implicit
 
-  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] = new Show[List[A]] {
+    override def show(a: List[A]): String = a.map(elem => ev.show(elem)).mkString(" ")
+  }
+
+  implicit def listShow2[A](implicit ev: Show[A]): Show[List[A]] = new Show[List[A]] {
+    override def show(a: List[A]): String = a.mkString(" ")
+  }
 
 
   // 2. Summoner (apply)
+  def apply[A](implicit ev: Show[A]): Show[A] = ev
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
     def show(implicit ev: Show[A]): String =
-      ???
+      ev.show(a)
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -39,16 +54,20 @@ object Show {
    *  @param begin. '[' in above example
    *  @param end. ']' in above example
    */
-  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    ???
+  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String = {
+    begin + list.mkString(separator) + end
+  }
 
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
-  
+  def fromJvm[A]: Show[A] = (a: A) => a.toString
+
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = new Show[A] {
+    override def show(a: A): String = f(a)
+  }
+
 
 }
