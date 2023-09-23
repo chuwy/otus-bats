5ad6ea90c14a09bed514ce864d6e58948258e58c
fjee <1102Fjee>
2023-09-23 20:18:25 +0300

    monad & show

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -1,13 +1,20 @@
 package me.chuwy.otusbats
 
 trait Monad[F[_]] extends Functor[F] { self =>
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] = flatten(map(fa)(a => f(a)))
 
   def point[A](a: A): F[A]
 
-  def flatten[A](fa: F[F[A]]): F[A]
+  def flatten[A](fa: F[F[A]]): F[A] = flatMap(fa)(a => a)
 }
 
 object Monad {
+  implicit def monadOption[T]: Monad[Option] = new Monad[Option] { thisMonad =>
 
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+
+    override def point[A](a: A): Option[A] = Option(a)
+
+    override def flatten[A](fa: Option[Option[A]]): Option[A] = fa.flatten
+  }
 }
--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -7,13 +7,10 @@ trait Show[A] {
 
 object Show {
 
-  // 1.1 Instances (`Int`, `String`, `Boolean`)
-
-
-  // 1.2 Instances with conditional implicit
-
-  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+  implicit def intShow: Show[Int] = fromFunction[Int](_ => toString)
+  implicit def stringShow: Show[String] = fromFunction[String](s => s)
+  implicit def booleanShow: Show[Boolean] = fromFunction[Boolean](_ => toString)
+  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] = fromFunction(a => a.mkString)
 
 
   // 2. Summoner (apply)
@@ -21,8 +18,7 @@ object Show {
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
-    def show(implicit ev: Show[A]): String =
-      ???
+    def show(implicit ev: Show[A]): String = ev.show(a)
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -39,16 +35,15 @@ object Show {
    *  @param begin. '[' in above example
    *  @param end. ']' in above example
    */
-  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    ???
+  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String = list.mkString(begin, separator, end)
 
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
+  def fromJvm[A]: Show[A] = _.toString
   
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = (a: A) => f(a)
 
 }
